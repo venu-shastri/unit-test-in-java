8c71dc36423a44d7ed2229fbb62dbcfbe108e9f9
Venugopal K <venu.shastri@gmail.com>
2024-06-27 06:02:15 +0530

    Update SoundexTest.java

--- test/java/CodeTestCoverJava/SoundexTest.java
+++ test/java/CodeTestCoverJava/SoundexTest.java
@@ -1,1 +1,18 @@
+package CodeTestCoverJava;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+public class SoundexTest {
+
+    @Test
+    public void testEmptyString() {
+        assertEquals("", Soundex.generateSoundex(""));
+    }
+
+    @Test
+    public void testSingleCharacter() {
+        assertEquals("A000", Soundex.generateSoundex("A"));
+    }
+
+  
