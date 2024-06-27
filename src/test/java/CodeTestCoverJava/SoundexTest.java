de5521b4a99cf61013051083d7e4fc756683f6af
Venugopal K <venu.shastri@gmail.com>
2024-06-27 06:19:14 +0530

    Create SoundexTest.java

---
+++ src/test/java/CodeTestCoverJava/SoundexTest.java
@@ -0,0 +1,17 @@
+package CodeTestCoverJava;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SoundexTest {
+
+    @Test
+    public void testEmptyString() {
+         assertTrue("", Soundex.generateSoundex(""));
+    }
+
+    @Test
+    public void testSingleCharacter() {
+         assertTrue("A000", Soundex.generateSoundex("A"));
+    }
